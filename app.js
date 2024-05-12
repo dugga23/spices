d86128f84b5005181b4620945f97e9afb0580246
dugga23 <164003474+dugga23@users.noreply.github.com>
2024-05-12 10:28:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ import bodyParser from "body-parser";
 import { config } from "dotenv";
 import userRoute from "./routes/userRoute.js";
 import { jwtoken } from "./middleware/jwtMiddleware.js";
-
+import cors from "cors";
 export const app = express();
 
 config();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/uploads',express.static('uploads'));
