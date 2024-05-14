d28f4272aeb30e362f52794c891c3b8f42ab8ac7
dugga23 <164003474+dugga23@users.noreply.github.com>
2024-05-14 18:35:46 +0530

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,37 +1,23 @@
 import express from "express";
-import {
-  signup,
-  login,
-  getUserDataForDashboard,
-  forgotpassword,
-} from "../controller/userController.js";
-import { contact } from "../controller/contactus.js";
-import { jwtoken } from "../middleware/jwtMiddleware.js";
-import {
-  imageupload,
-  deletePhoto,
-  updateCaption,
-} from "../controller/photoupload.js";
-import {uploadMiddleware}  from "../middleware/upload.js";
+import { signup,login,getUserDataForDashboard,forgotpassword} from "../controller/userController.js";
+import {contact} from "../controller/contactus.js"
+import {jwtoken} from "../middleware/jwtMiddleware.js"
+import {imageupload,deletePhoto,updateCaption} from "../controller/photoupload.js"
+import { upload } from "../middleware/upload.js";
 import { getproductDashboard } from "../controller/productDashboard.js";
 import { generateotp, verifyotp } from "../controller/otp.js";
 
+const router=express.Router();
 
-const router = express.Router();
-
-router.post("/sign-up", signup);
-router.post("/login", login);
-router.post("/contactus", contact);
-router.get("/dashboard", jwtoken, getUserDataForDashboard);
-router.post("/photoupload", uploadMiddleware, jwtoken, imageupload);
-router.delete("/photos/:photoId", jwtoken, deletePhoto);
-router.put("/photos/:photoId/caption", jwtoken, updateCaption);
-router.get("/productdashboard", getproductDashboard);
+router.post("/sign-up",signup);
+router.post("/login",login);
+router.post("/contactus",contact);
+router.get("/dashboard",jwtoken ,getUserDataForDashboard)
+router.get("/productdashboard",jwtoken,getproductDashboard);
 router.post("/otp", generateotp);
 router.post("/verifyotp", verifyotp);
-// router.put("/forgotpassword/:userId",forgotpassword);
-router.post("/forgotpassword", forgotpassword);
-
-//http://localhost:1025/api/v1/qr?url=https://rankyatra.com
-
+router.put("/forgotpassword/:userId",forgotpassword);
+router.post("/photoupload", upload, imageupload);
+router.delete("/photos/:photoId", deletePhoto);
+router.put('/photos/:photoId', updateCaption);
 export default router;
